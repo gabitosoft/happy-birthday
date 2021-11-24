831e0fb53f93d285467ab8171bc2efa6b34abe1c
Gabriel Delgado <gabriel.delgado@jalasoft.com>
2021-11-24 02:59:37 -0400

    Added changes to change value on checkbox

--- src/components/gift/gift.tsx
+++ src/components/gift/gift.tsx
@@ -10,13 +10,13 @@ const Gift = () => {
     <div>
       <h2>Sugerencia de regalos.</h2>
       <div className="gift-checkbox-items">
-        <Checkbox onChange={onChange} value="socks">
+        <Checkbox onChange={() => onChange('socks')}>
           <strong>Medias cortas deportivas</strong> <br/>(3 x 20 Bs)
         </Checkbox>
-        <Checkbox onChange={onChange} value="ball">
+        <Checkbox onChange={() => onChange('ball')} >
           <strong>Bolsa pelotas fronton</strong> <br/>(12 x 110 Bs)
         </Checkbox>
-        <Checkbox onChange={onChange} value="memory">
+        <Checkbox onChange={() => onChange('memory')}>
           <strong>Memoria Micro SD 16 GB</strong> <br/>(1 x 60 Bs)
         </Checkbox>
       </div>
